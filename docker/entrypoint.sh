1f842bfd1b999c12376cc2ea8735b4c742c800ec
Luis J. Manso <luis.manso@gmail.com>
2023-11-05 22:51:57 +0000

    code

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 
 export DISPLAY=:20
 Xvfb :20 -screen 0 1900x1000x16 &
-x11vnc xxx -display :20 -N -forever &
+x11vnc -passwd xxx -display :20 -N -forever &
 fluxbox &
 xinit &
 ln -s /usr/bin/python3.8 /usr/bin/python
