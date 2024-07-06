05f8852dba49516f6abe39e64a26f68fdd899394
Luis J. Manso <luis.manso@gmail.com>
2024-07-07 00:43:03 +0100

    tags

--- code/convertall.sh
+++ code/convertall.sh
@@ -5,10 +5,10 @@ for pre in $l; do
     nop=${pre/pre.pickle/}
     trj=${pre/pre/trj}
     echo "Joining $pre and $dat"
-    pytho3 join.py $nop 2> /dev/null
+    python3 join.py $nop #2> /dev/null
     echo "Generating JSON for $trj."
-    for mode in "joints" "circles" "polygons"; do
-        for video in "video" "nope"; do
+    for mode in "joints" "polygons" ; do # "polygons" "circles" "joints"
+        for video in "video"; do
             python3 sn3_to_json.py $trj $mode $video #2> /dev/null
         done
     done
